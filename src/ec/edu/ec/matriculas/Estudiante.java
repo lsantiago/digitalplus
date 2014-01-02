9930de874d05b7d1dc65fe7e1a02d5afa64e468e
lsantiago <lsquinones@gmail.com>
2014-01-01 19:17:18 -0500

    Inicio Clase Estudiante

---
+++ src/ec/edu/ec/matriculas/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.edu.ec.matriculas;
+
+/**
+ *
+ * @author lsantiago
+ */
+public class Estudiante {
+
+}
